a567a584a8d26fd528f39a9e0336081fcd53d456
Sercan Sebil <sercansebil95@googlemail.com>
2024-08-05 14:05:31 +0200

    add cancel/delete button

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./Color.css";
 
-export default function Color({ color }) {
+export default function Color({ color, onDeleteColor }) {
+  const [deleteColor, setDeleteColor] = useState("false");
+
+  function handleToggleDelete() {
+    setDeleteColor(!deleteColor);
+  }
+
   return (
     <div
       className="color-card"
@@ -12,6 +19,16 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {deleteColor ? (
+        <button onClick={handleToggleDelete}>DELETE</button>
+      ) : null}
+      {deleteColor ? null : (
+        <>
+          <p className="color-card-highlight">Really delete?</p>
+          <button onClick={handleToggleDelete}>CANCEL</button>
+          <button onClick={() => onDeleteColor(color.id)}>DELETE</button>
+        </>
+      )}
     </div>
   );
 }
